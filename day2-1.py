c410a3c57abf9417db3d64d7bc0dfd161f0f44c0
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2020-12-02 11:50:30 +0530

    Update day2-1.py

--- day2-1.py
+++ day2-1.py
@@ -5,10 +5,10 @@ def day2_1(data):
     count = 0
     for line in data:
         occurance = line.split(" ")[2].count(line.split(" ")[1][0])
-        Min = int(line.split(" ")[0].split('-')[0])
-        Max = int(line.split(" ")[0].split('-')[1])
+        low_lim = int(line.split(" ")[0].split('-')[0])
+        up_lim = int(line.split(" ")[0].split('-')[1])
 
-        if occurance in range(Min, Max + 1):
+        if occurance in range(low_lim, up_lim + 1):
             count += 1
 
     print(count)
