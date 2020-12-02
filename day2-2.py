3f0743065bf659586e1bcff3874a24621c2f8f81
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2020-12-02 11:55:21 +0530

    Update day2-2.py

--- day2-2.py
+++ day2-2.py
@@ -6,10 +6,7 @@ with open("input.txt") as f:
 def day2_2(data):
     count = 0
     for line in data:
-        rule1 = int(line.split(" ")[0].split('-')[0]) - 1
-        rule2 = int(line.split(" ")[0].split('-')[1]) - 1
-
-        if xor(line.split(" ")[2][rule1] == line.split(" ")[1][0], line.split(" ")[2][rule2] == line.split(" ")[1][0]):
+        if xor(line.split(" ")[2][int(line.split(" ")[0].split('-')[0]) - 1] == line.split(" ")[1][0], line.split(" ")[2][int(line.split(" ")[0].split('-')[1]) - 1] == line.split(" ")[1][0]):
             count += 1
     
     print(count)
