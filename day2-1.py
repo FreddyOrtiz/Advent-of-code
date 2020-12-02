7f519b21c8f6e11ccfdb2b44729f0adaec6c567a
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2020-12-02 11:53:14 +0530

    Update day2-1.py

--- day2-1.py
+++ day2-1.py
@@ -4,11 +4,8 @@ with open("input.txt") as f:
 def day2_1(data):
     count = 0
     for line in data:
-        occurance = line.split(" ")[2].count(line.split(" ")[1][0])
-        low_lim = int(line.split(" ")[0].split('-')[0])
-        up_lim = int(line.split(" ")[0].split('-')[1])
-
-        if occurance in range(low_lim, up_lim + 1):
+        this = line.split(" ")[2].count(line.split(" ")[1][0])
+        if this in range( int(line.split(" ")[0].split('-')[0]), ( int(line.split(" ")[0].split('-')[1]) + 1)):
             count += 1
 
     print(count)
