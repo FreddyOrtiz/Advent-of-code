7893363b6ef0b647f6684e36001cf25fee1bf747
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2020-12-02 19:03:11 +0530

    refactored

--- day-1/day-1-1.py
+++ day-1/day-1-1.py
@@ -2,8 +2,8 @@ with open("input.txt") as f:
 	a = [int(f.readline()[:-1]) for i in range(200)]
 
 for i in a:
-	for j in a:
-			if i + j == 2020:
-				print(i*j)
-			else:	
-				pass
+    for j in a:
+	if i + j == 2020:
+	    print(i*j)
+	else:	
+	    pass
