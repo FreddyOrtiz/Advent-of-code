80066707ff00faa5fb801b00bc866e7920d76867
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2020-12-03 18:13:28 +0530

    Update day-3-1.py

--- day-3/day-3-1.py
+++ day-3/day-3-1.py
@@ -1,1 +1,14 @@
-# day 3 part 1
+with open("input.txt") as f:
+    data = f.read().splitlines()
+
+dx = 0
+dy = 0
+trees = 0
+
+for line in data:
+    if line[dx % len(line)] == "#":
+        trees += 1
+    dx += 3
+    dy += 1
+
+print(trees)
