98d73fc972035ce4c1aba20a9502591a90fef013
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2020-12-06 21:21:53 +0530

    Update day-5-1.py

--- day-5/day-5-1.py
+++ day-5/day-5-1.py
@@ -1,1 +1,19 @@
-# solution to day 5 part 1
+def fun(text):
+    a = []
+    for i in text:
+        if i == 'F' or i == 'L':
+            a.append('0')
+        elif i == 'B' or i == 'R':
+            a.append('1')
+    return ''.join(a)
+
+with open("input.txt") as f:
+    data = f.read().splitlines()
+    b = []
+    for line in data:
+        row = int(fun(line[:7]), 2)
+        col = int(fun(line[-3:]),2)
+        index = row * 8 + col
+        b.append(index)
+
+print(max(b))
